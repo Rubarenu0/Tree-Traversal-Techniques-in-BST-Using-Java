629321152e6e87ac724f812657d8258e26757abc
Ruba Renugavathi A <142423853+Rubarenu0@users.noreply.github.com>
2024-08-08 09:43:04 +0530

    Add files via upload

---
+++ TreeTraversal.java
@@ -0,0 +1,52 @@
+class TreeNode{
+    int data;
+    TreeNode left,right;
+    TreeNode(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class TreeTraversal{ 
+    public void inorder(TreeNode root){
+        if(root==null)
+            return;
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    public void preorder(TreeNode root){
+        if(root==null)
+            return;
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public void postorder(TreeNode root){
+        if(root==null)
+            return;
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data+" ");
+    }
+public static void main(String[] args){
+    TreeNode root=new TreeNode(1);
+    root.left=new TreeNode(2);
+    root.right=new TreeNode(3);
+    root.left.left=new TreeNode(4);
+    root.left.right=new TreeNode(5);
+    root.left.right.left=new TreeNode(8);
+    root.right.left=new TreeNode(6);
+    root.right.right=new TreeNode(7);
+    root.right.right.left=new TreeNode(9);
+    root.right.right.right=new TreeNode(10);
+    TreeTraversal tree=new TreeTraversal();
+    System.out.println("Inorder of the BST");
+    tree.inorder(root);
+    System.out.println("");
+    System.out.println("preorder of the BST");
+    tree.preorder(root);
+    System.out.println("");
+    System.out.println("postorder of the BST");
+    tree.postorder(root);
+}
+}
